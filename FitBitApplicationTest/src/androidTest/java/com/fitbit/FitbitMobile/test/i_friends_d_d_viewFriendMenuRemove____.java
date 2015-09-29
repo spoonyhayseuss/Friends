aea2f5ce91b88c8af9b674e63326fedc7a5abb97
spoonyhayseuss <spoonyhayseuss@gmail.com>
2015-09-29 13:48:03 -0700

    Updated for 2.12 - removal of _ causing classNotFoundException.   Was:  "com.fitbit.onboarding.landing.LandingActivity_"  Now: "com.fitbit.onboarding.landing.LandingActivity"

--- FitBitApplicationTest/src/androidTest/java/com/fitbit/FitbitMobile/test/i_friends_d_d_viewFriendMenuRemove____.java
+++ FitBitApplicationTest/src/androidTest/java/com/fitbit/FitbitMobile/test/i_friends_d_d_viewFriendMenuRemove____.java
@@ -3,5 +3,5 @@ package com.fitbit.FitbitMobile.test;
 /**
  * Created by aaronwitherspoon on 9/6/15.
  */
-public class i_friends_d_d_viewFriendMenuRemove {
+public class i_friends_d_d_viewFriendMenuRemove____ {
 }
--- FitBitApplicationTest/src/androidTest/java/com/fitbit/FitbitMobile/test/i_friends_f_z_logOut.java
+++ FitBitApplicationTest/src/androidTest/java/com/fitbit/FitbitMobile/test/i_friends_f_z_logOut.java
@@ -9,7 +9,7 @@ import com.robotium.solo.Solo;
 public class i_friends_f_z_logOut extends ActivityInstrumentationTestCase2 {
     private Solo solo;
 
-    private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.fitbit.onboarding.landing.LandingActivity_";
+    private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.fitbit.onboarding.landing.LandingActivity";
 
     private static Class<?> launcherActivityClass;
     static{
@@ -48,7 +48,7 @@ public class i_friends_f_z_logOut extends ActivityInstrumentationTestCase2 {
         solo.clickOnText(java.util.regex.Pattern.quote("Log Out"));
 
 
-        assertTrue("LandingActivity_ is not found!", solo.waitForActivity("LandingActivity_"));
+        assertTrue("LandingActivity is not found!", solo.waitForActivity("LandingActivity"));
 
         solo.sleep(500);
 
